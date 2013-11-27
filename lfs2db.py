3a77c7e895c00ced1fe6af6897dbe6bc2ce22380
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2013-11-27 10:52:22 +0200

    implementing split logic

--- lfs2db.py
+++ lfs2db.py
@@ -72,7 +72,9 @@ class stock_move_split_bsm(osv.osv_memory):
                 '''
                 # mark data as used
                 self.pool.get('bsm.data').write(cr, uid, bsm.id, {'bsm_used': True})
-                self.pool.get('stock.move').write(cr, uid, moves[0].id, {'bsm_id':bsm})
+                prodlot_obj = self.pool.get('stock.production.lot')
+                prodlot_obj.write(cr, uid, moves[0].prodlot_id.id, {'bsm_id': bsm})
+                #self.pool.get('stock.move').write(cr, uid, moves[0].id, {'bsm_id':bsm})
                 #prodlot_obj.write(cr, uid, moves[0].prodlot_id.id, {'bsm_id': bsm})
                 print 'done'
         return True
