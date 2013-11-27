98a7253de381406bc899c4a0ccc5f93d4645419c
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2013-11-27 12:30:56 +0200

    implementing split logic

--- lfs2db.py
+++ lfs2db.py
@@ -43,11 +43,11 @@ class stock_move_split_bsm(osv.osv_memory):
     def _select_bsm_rows(self, cr, uid, context=None):
         print '_select_bsm_rows'
         obj = self.pool.get('bsm.data')
-        ids = obj.search(cr, uid, [('bsm_used', '=', False)])
+        ids = obj.search(cr, uid, args=[('bsm_used', '=', False)])
         print 'found unused bsm numbers: ' + str(ids)
-        res = obj.read(cr, uid, ids, ['bsm_imei_code', 'bsm_product_code'], context)
+        res = obj.read(cr, uid, ids, ['id', 'bsm_imei_code', 'bsm_product_code'], context)
         print str(res)
-        res = [(r['bsm_imei_code'], r['bsm_product_code']) for r in res]
+        res = [(r['bsm_imei_code'], r['bsm_product_code'], r['id']) for r in res]
         print str(res)
         return res
     
